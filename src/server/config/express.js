4b41864b2efc7307ed505277b28f1dd64897f59c
Jair Pérez <jair.perez@outlook.com>
2017-02-15 12:35:14 -0500

    bin

--- src/server/config/express.js
+++ src/server/config/express.js
@@ -30,9 +30,23 @@ app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/../../public/index.html');
 });
 
-// Listen =============================
-var env = process.env;
-app.listen(3000);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 //App export =========================
 module.exports = app;
